0f8b2e941dde976c1314bb3ac960d59022123c40
Restyled.io <commits@restyled.io>
2021-10-04 08:29:56 +0000

    Restyled by black

--- BucketSort.py
+++ BucketSort.py
@@ -32,7 +32,6 @@ def bucketSort(x):
     return x
 
 
-x = [0.897, 0.565, 0.656,
-     0.1234, 0.665, 0.3434]
+x = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434]
 print("Sorted Array is")
 print(bucketSort(x))
