0eb247724101845fc20f22d4a6464259ccf9af16
tusharsaxenabdn <30533353+tusharsaxenabdn@users.noreply.github.com>
2021-10-10 13:27:05 +0530

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,52 +1,56 @@
-# Python program for implementation of MergeSort
-def mergeSort(arr):
-	if len(arr) > 1:
+def merge(arr, l, m, r):
+n1 = m - l + 1
+n2 = r - m
 
-		mid = len(arr)//2
-		L = arr[:mid]
-		R = arr[mid:]
-		mergeSort(L)
-		mergeSort(R)
+L = [0] * (n1)
+R = [0] * (n2)
 
-		i = j = k = 0
+for i in range(0, n1):
+L[i] = arr[l + i]
 
-	
-		while i < len(L) and j < len(R):
-			if L[i] < R[j]:
-				arr[k] = L[i]
-				i += 1
-			else:
-				arr[k] = R[j]
-				j += 1
-			k += 1
+for j in range(0, n2):
+R[j] = arr[m + 1 + j]
 
-	
-		while i < len(L):
-			arr[k] = L[i]
-			i += 1
-			k += 1
 
-		while j < len(R):
-			arr[k] = R[j]
-			j += 1
-			k += 1
+i = 0
+j = 0
+k = l
 
+while i < n1 and j < n2:
+if L[i] <= R[j]:
+arr[k] = L[i]
+i += 1
+else:
+arr[k] = R[j]
+j += 1
+k += 1
+
+while i < n1:
+arr[k] = L[i]
+i += 1
+k += 1
 
 
 
-def printList(arr):
-	for i in range(len(arr)):
-		print(arr[i], end=" ")
-	print()
+while j < n2:
+arr[k] = R[j]
+j += 1
+k += 1
+
+def mergeSort(arr, l, r):
+if l < r:
+
+
+m = l+(r-l)//2
+
+mergeSort(arr, l, m)
+mergeSort(arr, m+1, r)
+merge(arr, l, m, r)
+
+arr = [12, 11, 13, 5, 6, 7]
+n = len(arr)
 
 
 
-if __name__ == '__main__':
-	arr = [12, 11, 13, 5, 6, 7]
-	print("Given array is", end="\n")
-	printList(arr)
-	mergeSort(arr)
-	print("Sorted array is: ", end="\n")
-	printList(arr)
 
-
+mergeSort(arr, 0, n-1)
